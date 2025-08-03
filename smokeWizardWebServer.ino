69a726a12db711bde6620eaaec1deba1dad38b63
jkan1001 <jaakko1107@gmail.com>
2025-08-03 18:42:00 -0500

    Moved to Arduino, imp. ESP32 WebSocket
    Plugins for server code were more manageable in Arduino than VSCode/PlatformIO; also implements WebSocket and provides basic website components

---
+++ smokeWizardWebServer.ino
@@ -0,0 +1,155 @@
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete instructions at https://RandomNerdTutorials.com/esp32-websocket-server-sensor/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+// #include <Adafruit_BME280.h>
+// #include <Adafruit_Sensor.h>
+
+// Replace with your network credentials
+const char* ssid = "Frogsaredope";
+const char* password = "sally101";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Create a WebSocket object
+AsyncWebSocket ws("/ws");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1000;
+
+// Create a sensor object
+// Adafruit_BME280 bme;
+
+// Create sensor dummy variables
+int mySmokerTemp = 25;
+int myMeatTemp = 18;
+int myHumidity = 100;
+int myJuice = 100;
+int myFuel = 100;
+
+// Init BME280
+void initBME(){
+  // if (!bme.begin(0x76)) {
+  //   Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  //   while (1);
+  // }
+  Serial.println("Faking the BME sensor init");
+}
+
+// Get Sensor Readings and return JSON object
+String getSensorReadings(){
+  readings["smoker-temperature"] = String(mySmokerTemp);
+  readings["meat-temperature"] = String(myMeatTemp);
+  readings["humidity"] =  String(myHumidity);
+  readings["spritz-juice-percent"] =  String(myJuice);
+  readings["fuel-supply-percent"] =  String(myFuel);
+  mySmokerTemp = mySmokerTemp + 1;
+  myMeatTemp = myMeatTemp + 1;
+  myHumidity = myHumidity + 1;
+  myJuice = myJuice + 1;
+  myFuel = myFuel + 1;
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+// Initialize LittleFS
+void initLittleFS() {
+  if (!LittleFS.begin(true)) {
+    Serial.println("An error has occurred while mounting LittleFS");
+  }
+  Serial.println("LittleFS mounted successfully");
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void notifyClients(String sensorReadings) {
+  ws.textAll(sensorReadings);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    //data[len] = 0;
+    //String message = (char*)data;
+    // Check if the message is "getReadings"
+    //if (strcmp((char*)data, "getReadings") == 0) {
+      //if it is, send current sensor readings
+      String sensorReadings = getSensorReadings();
+      Serial.print(sensorReadings);
+      notifyClients(sensorReadings);
+    //}
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void setup() {
+  Serial.begin(115200);
+  initBME();
+  initWiFi();
+  initLittleFS();
+  initWebSocket();
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    String sensorReadings = getSensorReadings();
+    Serial.print(sensorReadings);
+    notifyClients(sensorReadings);
+    lastTime = millis();
+  }
+  ws.cleanupClients();
+}
